4fd0a0543df636b4ed4afbd2c4cd29fa6562a703
zehratas <108723059+zehratas@users.noreply.github.com>
2022-07-06 12:15:12 +0300

    Add version endpoint (#2)

---
+++ handler.go
@@ -0,0 +1,21 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+type Handler struct {
+}
+
+type Versions struct {
+	Ios     string `json:"ios"`
+	Android string `json:"and"`
+	Huawei  string `json:"hua"`
+}
+
+func (h *Handler) GetVersion(c *fiber.Ctx) error {
+	versions := Versions{
+		Ios:     "1.0.0",
+		Android: "1.0.0",
+		Huawei:  "1.0.0",
+	}
+	return c.Status(fiber.StatusOK).JSON(versions)
+}
